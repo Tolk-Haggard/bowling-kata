1ad97894123fede35baac4d25498babc99261528
Brian Haggard <brihagg@gmail.com>
2018-05-02 07:16:07 -0500

    pairing interview

--- src/app/score-keeper.service.spec.ts
+++ src/app/score-keeper.service.spec.ts
@@ -28,4 +28,31 @@ describe('ScoreKeeperService', () => {
 
     expect(testObject.calculateScore()).toEqual(0);
   });
+
+  it('should score a game with a spare in every frame', () => {
+    for (i = 0; i < 20; i++) {
+      if (i % 2 === 0) {
+        testObject.rollBall(6);
+      } else {
+        testObject.rollBall(4);
+      }
+    }
+    testObject.rollBall(10);
+
+    expect(testObject.calculateScore()).toEqual(164);
+  });
+
+  it('should score a game with a spare', () => {
+    for (i = 0; i < 20; i++) {
+      if (i === 7) {
+        testObject.rollBall(6);
+      } else if (i === 15) {
+        testObject.rollBall(6);
+      } else {
+        testObject.rollBall(4);
+      }
+    }
+
+    expect(testObject.calculateScore()).toEqual(92);
+  });
 });
--- src/app/score-keeper.service.ts
+++ src/app/score-keeper.service.ts
@@ -3,14 +3,46 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ScoreKeeperService {
 
-  constructor() { }
+  frame_started: boolean;
+  previous_spare: boolean;
+
+  total_score: number;
+  roll1: number;
+  current_frame: number;
+
+  constructor() {
+    this.total_score = 0;
+    this.roll1 = 0;
+    this.frame_started = false;
+    this.previous_spare = false;
+    this.current_frame = 1;
+  }
 
   public rollBall(pins: number) {
+    this.total_score += pins;
 
+    if (this.frame_started) {
+      if (pins + this.roll1 === 10) {
+        this.previous_spare = true;
+      }
+
+      this.frame_started = false;
+      this.current_frame++;
+    } else { // This is the first roll of the frame
+
+      this.frame_started = true;
+      this.roll1 = pins;
+
+      if ((this.previous_spare === true) &&
+        (this.current_frame <= 10)) {
+        this.total_score += pins;
+        this.previous_spare = false;
+      }
+    }
   }
 
   public calculateScore(): number {
-    return -1;
+    return this.total_score;
   }
 
 }
